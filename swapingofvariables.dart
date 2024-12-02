f5643e1d9672c998a2f75a1cda32c69e51469a7b
MUHAMMAD HASSAAN TAHIR <hassaantahir991@gmail.com>
2024-12-02 21:12:42 +0500

    Create swapingofvariables.dart

---
+++ swapingofvariables.dart
@@ -0,0 +1,18 @@
+void main() {
+  // In this program we will swap values of two variables
+
+  int a, b;
+  a = 20;
+  b = 15;
+
+  print('Before swaping');
+  print('value of a: $a');
+  print('value of b: $b');
+
+  a = a + b; // 20+15 = 35
+  b = a - b; // 35-15 = 20
+
+  print('After swaping');
+  print('value of a: $a');
+  print('value of b: $b');
+}
